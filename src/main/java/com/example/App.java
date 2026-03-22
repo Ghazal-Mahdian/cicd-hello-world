a2a10609aedc035a1b56d6540d58dc255a0f7757
ghazal <gmahdian1@toromail.csudh.edu>
2026-03-22 15:18:48 -0700

    Initial commit - Hello World Java app

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World from CI/CD Pipeline!");
+    }
+
+    public static String getMessage() {
+        return "Hello World from CI/CD Pipeline!";
+    }
+}
