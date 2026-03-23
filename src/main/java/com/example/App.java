87af8994fd4ebf73223c05a5aedac367a4334c35
Ghazal-Mahdian <gmahdian1@toromail.csudh.edu>
2026-03-23 14:46:06 -0700

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,9 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello World from CI/CD Pipeline!");
+        
         while (true) {
             Thread.sleep(10000); 
         }
