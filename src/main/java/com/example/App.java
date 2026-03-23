c6b86c05289591843173f48e2babd81df699ffa2
Ghazal-Mahdian <gmahdian1@toromail.csudh.edu>
2026-03-23 14:43:45 -0700

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,9 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World from CI/CD Pipeline!");
+        while (true) {
+            Thread.sleep(10000); 
+        }
     }
 
     public static String getMessage() {
